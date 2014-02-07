7395c30f3f8c99da569cdfd3a372cbacbe14395a
MorganDawe <mdawe@discoverygarden.com>
2014-02-07 20:04:57 +0000

    Document the oddness that is the jquery dependancies in bookreader.

--- js/islandora_internet_archive_bookreader.js
+++ js/islandora_internet_archive_bookreader.js
@@ -1,8 +1,13 @@
 /**
+ * Defines initializing/attaching the Book Reader to the
  * @file
- *
- * Defines initializing/attaching the Book Reader to the defined element.
+ * defined element.
  */
+
+// noConflict is here to allow for compatibility between jQuery 1.5 and jquery 1.7.
+// JS loaded prior to this point in the bookreader requires jQuery 1.5, and further
+// execution requires jQuery 1.7 or higher. 
+// TODO: Figure out what library is the culprate.
 jQuery.noConflict(true);
 (function ($) {
   Drupal.behaviors.islandoraInternetArchiveBookReader = {
