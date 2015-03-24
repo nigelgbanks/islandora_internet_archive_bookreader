da46e5e3df3454215424141de6882c99020a59cd
qadan <daitken@discoverygarden.ca>
2015-03-24 11:18:07 -0300

    make it toggleable

--- js/islandora_book_reader.js
+++ js/islandora_book_reader.js
@@ -219,13 +219,18 @@
   IslandoraBookReader.prototype.getPageURI = function(index, reduce, rotate) {
     if (typeof this.settings.pages[index] != 'undefined') {
       var resource_uri = null;
-      $.ajax({
-        url: this.settings.tokenUri.replace('PID', this.settings.pages[index].pid),
-        async: false,
-        success: function(data, textStatus, jqXHR) {
-          resource_uri = data;
-        }
-      });
+      if (this.settings.useBackupUri == true) {
+        $.ajax({
+          url: this.settings.tokenUri.replace('PID', this.settings.pages[index].pid),
+          async: false,
+          success: function(data, textStatus, jqXHR) {
+            resource_uri = data;
+          }
+        });
+      }
+      else {
+        resource_uri = this.settings.pages[index].uri
+      }
       return this.getDjatokaUri(resource_uri);
     }
   }
